ebdf1fda5361fa4df9b1e47671362f7100540800
dmswo159 <dmswo159@gmail.com>
2023-06-21 17:49:13 +0900

    텍스트폼필드

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -10,12 +10,194 @@ class LoginSignupScreen extends StatefulWidget {
 
 class _LoginSignupScreenState extends State<LoginSignupScreen> {
   bool isSingupScreen = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Palette.backgroundColor,
+      // 겹겹이 쌓을 수 있게하는 위젯 / 위젯들을 원하는 곳에 위치 시킬 수 있다
       body: Stack(
-
+        children: [
+          // 원하는 곳에 위치 시킬 수 있다. (상.하.좌.우)
+          Positioned(
+            top: 0,
+            right: 0,
+            left: 0,
+            child: Container(
+              height: 300,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('image/red.jpg'),
+                  fit: BoxFit.fill,
+                ),
+              ),
+              child: Container(
+                padding: EdgeInsets.only(top: 90, left: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // RichText가 텍스트를 쪼개서 원하는 곳에 디자인 할 수 있게 해주는거
+                    RichText(
+                      text: TextSpan(
+                        text: 'Welcome',
+                        style: TextStyle(
+                          letterSpacing: 1.0,
+                          fontSize: 25,
+                          color: Colors.white,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: ' to Yummy chat!',
+                            style: TextStyle(
+                              letterSpacing: 1.0,
+                              fontSize: 25,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      'Singup to continue',
+                      style: TextStyle(
+                        letterSpacing: 1.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ), // 배경
+          Positioned(
+            top: 180,
+            child: Container(
+              padding: EdgeInsets.all(20.0),
+              height: 280,
+              width: MediaQuery.of(context).size.width - 40,
+              margin: EdgeInsets.symmetric(horizontal: 20.0),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.3),
+                      blurRadius: 15,
+                      spreadRadius: 5,
+                    )
+                  ]),
+              //실제 디바이스의 width 길이를 구해준다
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      // Login 텍스트
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            isSingupScreen = false;
+                          });
+                        },
+                        child: Column(
+                          children: [
+                            Text(
+                              'Log In',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: !isSingupScreen
+                                    ? Palette.activeColor
+                                    : Palette.textColor1,
+                              ),
+                            ),
+                            if (!isSingupScreen)
+                              //inline if - dart 2.3부터 도입 한 컬럼 위젯 내에 요소들을 어떤 예외적인 조건을 보다 쉽고 명확하게 지정해 줄수 있는 기능
+                              Container(
+                                height: 2,
+                                width: 55,
+                                color: Colors.orange,
+                              ),
+                          ],
+                        ),
+                      ),
+                      // SignUp 텍스트
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            isSingupScreen = true;
+                          });
+                        },
+                        child: Column(
+                          children: [
+                            Text(
+                              'Sign Up',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: isSingupScreen
+                                    ? Palette.activeColor
+                                    : Palette.textColor1,
+                              ),
+                            ),
+                            if (isSingupScreen)
+                              Container(
+                                height: 2,
+                                width: 65,
+                                color: Colors.orange,
+                              ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  // 회원정보 입력 구간 **********************************
+                  // 텍스트 필드에디터는 많아지면 controller가 많아지고 복잡해진다
+                  // 그래서 텍스트 폼 필드를 사용한다.
+                  Container(
+                    child: Form(
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            decoration: InputDecoration(
+                              prefixIcon: Icon(
+                                Icons.account_circle,
+                                color: Palette.iconColor,
+                              ),
+                              // enableBoarder만 쓰면 텍스트 필드 클릭시 Boder가 사라진다
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Palette.textColor1,
+                                ),
+                                borderRadius: BorderRadius.circular(35),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Palette.textColor1,
+                                ),
+                                borderRadius: BorderRadius.circular(35),
+                              ),
+                              hintText: 'User name',
+                              hintStyle: TextStyle(
+                                fontSize: 14,
+                                color: Palette.textColor1,
+                              ),
+                              contentPadding: EdgeInsets.all(10),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
