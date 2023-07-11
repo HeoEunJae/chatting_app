9ee12d38513400b7667e065d0c678487357e55f0
dmswo159 <dmswo159@gmail.com>
2023-07-11 10:36:34 +0900

    파이어베이스데이터가져오기

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-    
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
-    
+
   @override
   State<ChatScreen> createState() => _ChatScreenState();
 }
-    
+
 class _ChatScreenState extends State<ChatScreen> {
   final _authentication = FirebaseAuth.instance;
   User? loggedUser;
@@ -32,19 +33,47 @@ class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(onPressed: (){
-            _authentication.signOut();
-            Navigator.pop(context);
-          }, icon: Icon(Icons.logout, color: Colors.white),),
-        ],
-        title: Text('Chat screen'),
-      ),
-      body: Center(
-        child: Text('Chat screen'),
-    ),
-    );
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              onPressed: () {
+                _authentication.signOut();
+                Navigator.pop(context);
+              },
+              icon: Icon(Icons.logout, color: Colors.white),
+            ),
+          ],
+          title: Text('Chat screen'),
+        ),
+        body: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection('chats/60eo1lpvjS0FtWZWCaOO/message') // 데이터베이스의 주소
+              .snapshots(),
+          builder: (BuildContext context,
+              AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            // 스트림으로 전달받은 데이터에 접근이 가능 하다
+            final docs =
+                snapshot.data!.docs; //데이터 필드에는 모든 데이터가있기때문에 data뒤에 느낌표를 붙여준다
+            return ListView.builder(
+              itemCount: docs.length,
+              // 문서내의 모든 데이터의 개수( 채팅앱처럼 개수가 매번 바뀌게되면 의미가 없어진다)
+              itemBuilder: (context, index) {
+                return Container(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    docs[index]['text'],
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                );
+              },
+            );
+          },
+        ));
   }
 }
     
